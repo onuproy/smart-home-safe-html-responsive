dddbc1cf7703fd0b1719d9202847167df43c8469
onuproy <anupme01@gmail.com>
2021-07-26 10:37:01 +0600

    Home html full complete and upload

---
+++ assets/js/main.js
@@ -0,0 +1,26 @@
+$(document).ready(function() {
+
+
+	// Mobile Menu
+	$('.nav-bars').click(function(){
+		$('.menu_area ul').slideToggle(700);
+	});
+
+	//Aso 
+    AOS.init({
+        duration: 1000,
+        delay: 50,
+        once: true
+     });
+
+
+
+
+});
+
+
+
+
+        
+
+
